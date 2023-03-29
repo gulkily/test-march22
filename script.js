86eb376ef575bfe2e03efd00143b1971f6fbd7b2
Ilya Gulko <1530699+gulkily@users.noreply.github.com>
2023-03-29 11:40:02 -0400

    add code for when style already in page

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ function selectStyle(styleName) {
   var cssId = 'myCss';  // you could encode the css path itself to generate id..
   if (!document.getElementById(cssId))
   {
-      var head  = document.getElementsByTagName('head')[0];
-      var link  = document.createElement('link');
-      link.id   = cssId;
-      link.rel  = 'stylesheet';
-      link.type = 'text/css';
-      link.href = styleName;
-      link.media = 'all';
-      head.appendChild(link);
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = styleName;
+    link.media = 'all';
+    head.appendChild(link);
+  }
+  else
+  {
+    var link = document.getElementById(cssId);
+    link.href = styleName;
   }
 }
