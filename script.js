10e2dfac75e672db259b88e8f5f5ecd74f9fb64f
Ilya Gulko <1530699+gulkily@users.noreply.github.com>
2023-03-29 11:30:55 -0400

    add code to insert style into page

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function selectStyle(styleName) {
-  alert(styleName);
+  // check for existing <link> on page, change its src attribute
+  // if existing not found, create <link> element on page, set its src attribute
+
+  var cssId = 'myCss';  // you could encode the css path itself to generate id..
+  if (!document.getElementById(cssId))
+  {
+      var head  = document.getElementsByTagName('head')[0];
+      var link  = document.createElement('link');
+      link.id   = cssId;
+      link.rel  = 'stylesheet';
+      link.type = 'text/css';
+      link.href = styleName;
+      link.media = 'all';
+      head.appendChild(link);
+  }
 }
